a389d2a3f0cef6d7400ed7ffb1b3fbc53db2c3ce
MarwenLabidi <marwenlabidi.dev@gmail.com>
2022-12-04 20:16:12 +0100

    🚧 Work in progress : setting module federation plugin

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),federation({
-    name: "header",
+    name: "headerSubApp",
     filename: "remoteEntry.js",
     exposes: {
-      './Header': './src/App.jsx',
+      './HeaderApp': './src/App',
     },
     shared: ["react", "react-dom"],
   })],
